6fcad279980b88cb5803cb227b2b7d3ad099e02a
Noah Porfido <noah@menon.group>
2023-02-05 15:09:06 +0100

    init

---
+++ src/core/data/images.ts
@@ -0,0 +1,53 @@
+import type image from '../models/image';
+
+export const imageData: image[] = [
+	{
+		year: '2000',
+		mass: 'asdas',
+		title: 'asdsa',
+		material: 'asdasd',
+		url: '/images/2.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/1.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/3.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/4.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/5.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/6.jpg'
+	},
+	{
+		year: '2002',
+		mass: '12312',
+		title: '432423',
+		material: '2343',
+		url: '/images/7.jpg'
+	}
+];
---
+++ src/core/models/image.ts
@@ -0,0 +1,7 @@
+export default interface Image {
+	readonly title: string;
+	readonly year: string;
+	readonly material: string;
+	readonly mass: string;
+	readonly url: string;
+}
